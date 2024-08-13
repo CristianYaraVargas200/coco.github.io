8e1a5afe8e543cb1d9479325fdbb1508b692c847
CristianYaraVargas200 <126600513+CristianYaraVargas200@users.noreply.github.com>
2024-08-13 08:20:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# Configuración de la base de datos (ajusta según tus credenciales)
+# Configuración de la base de datos
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'popo'
 app.config['MYSQL_PASSWORD'] = 'popo'
@@ -11,21 +11,23 @@ app.config['MYSQL_DB'] = 'coco'
 
 mysql = MySQL(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return render_template('form.html')
+
+@app.route('/POST', methods=['POST'])
+def submit():
     if request.method == 'POST':
-        # Obtener los datos del formulario
         nombre = request.form['nombre']
         apellido = request.form['apellido']
 
-        # Insertar los datos en la base de datos
-        cursor = mysql.connection.cursor()
-        cursor.execute("INSERT INTO form (nombre, apellido) VALUES (%s, %s)", (nombre, apellido))
+        # Inserta los datos en la base de datos
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO form (nombre, apellido) VALUES (%s, %s)", (nombre, apellido))
         mysql.connection.commit()
-        cursor.close()
+        cur.close()
+
         return 'Datos guardados exitosamente'
-    else:
-        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
